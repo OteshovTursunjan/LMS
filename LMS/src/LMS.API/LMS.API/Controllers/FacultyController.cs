76667642ce1f5dcf582ab18638e2d452d5c6f899
Tony <tursunoteshov@gmail.com>
2025-04-03 22:40:11 +0500

    FacultyDone

--- LMS/src/LMS.API/LMS.API/Controllers/FacultyController.cs
+++ LMS/src/LMS.API/LMS.API/Controllers/FacultyController.cs
@@ -1,5 +1,7 @@
 ﻿using LMS.Application.DTO.Create;
+using LMS.Application.DTO.Update;
 using LMS.Application.Feature.Faculty.Command;
+using LMS.Application.Feature.Faculty.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,12 +18,37 @@ public class FacultyController : Controller
     {
         this.mediator = mediator;
     }
+    [HttpGet("GetFaculty")]
+    public async Task<IActionResult> GetFaculty(Guid id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        var res = await mediator.Send(new GetFacultyQueries(id));
+        return Ok(res);
+
+    }
     [HttpPut("CreateFaculty")]
     public async Task<IActionResult> CreateFaculty(FacultyCreateModel facultyCreateModel)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
         var res = await mediator.Send(new CreateFacultyCommand(facultyCreateModel));
+        return Ok(res);
+    }
+    [HttpPost("UpdateFaculty")]
+    public async Task<IActionResult> UpdateFaculty(FacultyUpdateModel facultyUpdateModel)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        var res = await mediator.Send(new UpdateFacultyCommand(facultyUpdateModel));
+        return Ok(res);
+    }
+    [HttpDelete("DeletFaculty")]
+    public async Task<IActionResult> DeleteFaculty(Guid id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        var res = await mediator.Send(new DeleteFacultyCommand(id));    
         return Ok(res);
     }
   
