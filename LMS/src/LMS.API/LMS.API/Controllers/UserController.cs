4b581b394464efec5d9b5649d75f3ddb03cf12d9
Tony <tursunoteshov@gmail.com>
2025-05-29 18:03:37 +0500

    mer

--- LMS/src/LMS.API/LMS.API/Controllers/UserController.cs
+++ LMS/src/LMS.API/LMS.API/Controllers/UserController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace LMS.API.Controllers
 {
 //S
+/// <summary>
+/// </summary>
     public class UserController : Controller
     {
         private readonly IMediator mediator;
